b852328d23f7540a12ff9583711b9677acd9b8e7
dima <dimasilolil@gmail.com>
2020-09-23 20:06:04 +0600

    second css kit

--- src/contentSwitch.js
+++ src/contentSwitch.js
@@ -1,13 +1,25 @@
 function listView() {
-    // for (var i  = document.getElementsByClassName("mainRectangle"); i != null; i = document.getElementsByClassName("mainRectangle")) {
-        let element = document.getElementById("mainRectangle");
-        // i.classList.add('mainRectangle');
-    // }
+    let content = document.getElementById("content");
+    let elements = document.getElementsByClassName("secondRectangle");
+    Array.from(elements).forEach(
+        element => {
+            element.classList.remove("secondRectangle");
+            element.classList.add("mainRectangle");
+        }
+    )
+    content.classList.remove("contentGrid");
+    content.classList.add("contentList");
 }
 
 function gridView() {
-    for (let i  = document.getElementsByClassName("mainRectangle"); i != null; i = document.getElementsByClassName("mainRectangle")) {
-        // let element = document.getElementById("mainRectangle");
-        i.classList.remove('mainRectangle');
-    }
+    let content = document.getElementById("content");
+    let elements = document.getElementsByClassName("mainRectangle");
+    Array.from(elements).forEach(
+        element => {
+            element.classList.remove("mainRectangle");
+            element.classList.add("secondRectangle");
+        }
+    )
+    content.classList.remove("contentList");
+    content.classList.add("contentGrid");
 }
